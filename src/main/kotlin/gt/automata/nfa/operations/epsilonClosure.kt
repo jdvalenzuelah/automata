29a42d5c37096c49fc3f284235550f922e060870
Josue Valenzuela <val171001@uvg.edu.gt>
2021-03-06 19:37:40 -0600

    fix epsilon closure

--- src/main/kotlin/gt/automata/nfa/operations/epsilonClosure.kt
+++ src/main/kotlin/gt/automata/nfa/operations/epsilonClosure.kt
@@ -3,10 +3,15 @@ package gt.automata.nfa.operations
 import gt.automata.IState
 import gt.automata.nfa.NonDeterministicFiniteAutomata
 
+fun interface EClosure<S, I> {
+    fun eClosure(dfa: NonDeterministicFiniteAutomata<S, I>, vararg t: IState<S>): Collection<IState<S>>
+}
+
 class EpsilonClosure<S, I>(
     private val epsilonValue: I
-) {
-    fun eClosure(dfa: NonDeterministicFiniteAutomata<S, I>, vararg t: IState<S>): Collection<IState<S>> {
+) : EClosure<S, I> {
+
+    override fun eClosure(dfa: NonDeterministicFiniteAutomata<S, I>, vararg t: IState<S>): Collection<IState<S>> {
         val eClosure = mutableListOf<IState<S>>().apply { addAll(t) }
         val stateStack = ArrayDeque<IState<S>>().apply { addAll(t) }
 
@@ -14,11 +19,13 @@ class EpsilonClosure<S, I>(
             val currentState = stateStack.removeLast()
 
             dfa.move(currentState, epsilonValue)?.forEach { destState ->
-                if(destState !in eClosure) eClosure.add(destState)
-                stateStack.addLast(destState)
+                if(destState !in eClosure) {
+                    eClosure.add(destState)
+                    stateStack.addLast(destState)
+                }
             }
         }
-
         return eClosure
     }
+
 }
