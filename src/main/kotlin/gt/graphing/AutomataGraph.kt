9a4abe0a90e9744ac908495b8f6bd684a9a10706
Josue Valenzuela <val171001@uvg.edu.gt>
2021-03-06 19:43:29 -0600

    Improve graph formats and update readme example

--- src/main/kotlin/gt/graphing/AutomataGraph.kt
+++ src/main/kotlin/gt/graphing/AutomataGraph.kt
@@ -3,53 +3,62 @@ package gt.graphing
 import gt.automata.IState
 import gt.automata.dfa.DeterministicFiniteAutomata
 import gt.automata.nfa.NonDeterministicFiniteAutomata
-import guru.nidi.graphviz.attribute.Label
-import guru.nidi.graphviz.attribute.Rank
+import guru.nidi.graphviz.KraphvizContext
+import guru.nidi.graphviz.attribute.*
 import guru.nidi.graphviz.engine.Graphviz
 import guru.nidi.graphviz.graph
 import guru.nidi.graphviz.toGraphviz
+
+
 
 // TODO: Refactor transition table to avoid so many loops
 class AutomataGraph<T, U> {
 
     private data class Transitions<S, L>(val from: S, val to: S, val by: L)
 
+    private fun baseGraph(config: KraphvizContext.() -> Unit) = graph(directed = true) {
+        graph[Rank.dir(Rank.RankDir.LEFT_TO_RIGHT)]
+        "fake"[Style.INVIS]
+        config()
+    }
 
-    private fun <S, L> getTransitionsFromNFA(nfa: NonDeterministicFiniteAutomata<S, L>): List<Transitions<IState<S>, L>> {
-        return nfa.transitionTable.flatMap { (fromState, transitions) ->
-            transitions.flatMap { (action, toStates) ->
-                toStates.map { toState ->
-                    Transitions(fromState, toState, action)
+    fun graphFromNfa(nfa: NonDeterministicFiniteAutomata<T, U>): Graphviz {
+        return baseGraph {
+            nfa.states.forEach {
+                when (it) {
+                    nfa.initialState -> it.toString()[Style.BOLD]
+                    in nfa.finalStates -> it.toString()[Shape.DOUBLE_CIRCLE]
+                    else -> it.toString()
                 }
             }
-        }
-    }
 
-    private fun <S, L> getTransitionsFromDFA(dfa: DeterministicFiniteAutomata<S, L>): List<Transitions<IState<S>, L>> {
-        return dfa.transitionTable.flatMap { (fromState, transitions) ->
-            transitions.map { (action, toState) ->
-                Transitions(fromState, toState, action)
-            }
-        }
-    }
-
-    private fun <S, L> graph(transitions: List<Transitions<S, L>>): Graphviz {
-        // TODO: add double circle for final state `Shape.DOUBLE_CIRCLE`
-        return graph(directed = true) {
-            graph[Rank.dir(Rank.RankDir.LEFT_TO_RIGHT)]
-            transitions.forEach {
-                (it.from.toString() - it.to.toString())[Label.of(it.by.toString())]
+            "fake" - nfa.initialState.toString()
+            nfa.transitionTable.flatMap { (fromState, transitions) ->
+                transitions.flatMap { (action, toStates) ->
+                    toStates.map { toState ->
+                        (fromState.toString() - toState.toString())[Label.of(action.toString())]
+                    }
+                }
             }
         }.toGraphviz()
     }
 
-    fun graphFromNfa(nfa: NonDeterministicFiniteAutomata<T, U>): Graphviz {
-        val transitions = getTransitionsFromNFA(nfa)
-        return graph(transitions)
-    }
-
     fun graphFromDfa(dfa: DeterministicFiniteAutomata<T, U>): Graphviz {
-        val transitions = getTransitionsFromDFA(dfa)
-        return graph(transitions)
+        return baseGraph {
+            dfa.states.forEach {
+                when (it) {
+                    dfa.initialState -> it.toString()[Style.BOLD]
+                    in dfa.finalStates -> it.toString()[Shape.DOUBLE_CIRCLE]
+                    else -> it.toString()
+                }
+            }
+
+            "fake" - dfa.initialState.toString()
+            dfa.transitionTable.flatMap { (fromState, transitions) ->
+                transitions.map { (action, toState) ->
+                    (fromState.toString() - toState.toString())[Label.of(action.toString())]
+                }
+            }
+        }.toGraphviz()
     }
 }
