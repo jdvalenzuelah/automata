ac894e84011ded55198712dca3bd00394a06c9b3
Josue Valenzuela <val171001@uvg.edu.gt>
2021-03-13 21:14:12 -0600

    Add direct construction of regex to dfa

---
+++ src/main/kotlin/gt/automata/dfa/operations/SyntaxTreeToDeterministicFinateAutomata.kt
@@ -0,0 +1,78 @@
+package gt.automata.dfa.operations
+
+import gt.automata.dfa.DeterministicFiniteAutomata
+import gt.automata.dfa.models.DFA
+import gt.automata.dfa.models.TransitionTable
+import gt.automata.dfa.models.dfa
+import gt.automata.dfa.treeOperations.ISyntaxTree
+import gt.regex.element.Character
+import gt.regex.element.Augmented
+
+
+fun interface SyntaxTreeToDeterministicFinateAutomata : (ISyntaxTree) -> DeterministicFiniteAutomata<String, String>
+
+
+object SyntaxTreeToDfa : SyntaxTreeToDeterministicFinateAutomata {
+
+    private data class Marked(var marked: Boolean, val states: Collection<Int>)
+
+    override fun invoke(tree: ISyntaxTree): DeterministicFiniteAutomata<String, String> {
+        val charPos = tree.root.mapNotNull {
+            when(it.data) {
+                is Character -> it.data.char to it.position!!
+                is Augmented.EndMarker -> it.data.id to it.position!!
+                else -> null
+            }
+        }
+            .groupBy { it.first }
+
+        val start = tree.firstpos(tree.root)
+        val dStates = mutableListOf(Marked(false, start))
+        val dTran = mutableMapOf<Pair<Collection<Int>, String>, Collection<Int>>()
+        val endMarkerPos = charPos[Augmented.EndMarker.id]?.map { it.second } ?: emptyList()
+        val finalStates = mutableListOf<Collection<Int>>()
+
+        do {
+            val s = dStates.firstOrNull { !it.marked }
+
+            if(s != null) {
+                s.marked = true
+
+                if(s.states.any { it in endMarkerPos })
+                    finalStates.add(s.states)
+
+                for(a in tree.alphabet) {
+                    if(a == Augmented.EndMarker.id) continue
+                    val aPos = charPos[a] ?: emptyList()
+                    val u = aPos.filter { it.second in s.states }.flatMap { tree.followpos(it.second) }.toSet()
+
+                    if(dStates.none { it.states == u })
+                        dStates.add(Marked(false, u))
+
+                    dTran[s.states to a] = u
+                }
+            }
+        }while (s != null)
+
+        // TODO: Remove all the loops!
+        return dfa {
+
+            states {
+                dTran.forEach { (from, to) ->
+                    state { from.first.toString() }
+                    state { to.toString() }
+                }
+            }
+            initialState(start.toString())
+            finalStates(*finalStates.map { it.toString() }.toTypedArray())
+
+            transitions {
+                dTran.forEach { (from, to) ->
+                    from.first.toString() to to.toString() by from.second
+                }
+            }
+
+        }
+
+    }
+}
